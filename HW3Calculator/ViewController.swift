7289a2c9abc32fbafd8a0da2e8e8cf86bde986d6
Christian <tsounguc@mail.gvsu.edu>
2020-01-30 18:18:32 -0500

    Initial Commit

---
+++ HW3Calculator/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  HW3Calculator
+//
+//  Created by Xcode User on 1/30/20.
+//  Copyright © 2020 Xcode User. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ HW3CalculatorTests/HW3CalculatorTests.swift
@@ -0,0 +1,34 @@
+//
+//  HW3CalculatorTests.swift
+//  HW3CalculatorTests
+//
+//  Created by Xcode User on 1/30/20.
+//  Copyright © 2020 Xcode User. All rights reserved.
+//
+
+import XCTest
+@testable import HW3Calculator
+
+class HW3CalculatorTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
