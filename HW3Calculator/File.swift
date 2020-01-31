340e8d8c21cd12dceaa167a346f791d18a0e05f2
DrScatman <milleja1@mail.gvsu.edu>
2020-01-31 17:05:27 -0500

    partB

---
+++ HW3Calculator/File.swift
@@ -0,0 +1,9 @@
+//
+//  File.swift
+//  HW3Calculator
+//
+//  Created by Xcode User on 1/31/20.
+//  Copyright © 2020 Xcode User. All rights reserved.
+//
+
+import Foundation
---
+++ HW3Calculator/ViewController2.swift
@@ -0,0 +1,9 @@
+//
+//  ViewController2.swift
+//  HW3Calculator
+//
+//  Created by Xcode User on 1/31/20.
+//  Copyright © 2020 Xcode User. All rights reserved.
+//
+
+import Foundation
