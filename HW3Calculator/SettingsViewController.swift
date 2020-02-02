42d681e204e295788a4faa0390ae3c6885aa2961
DrScatman <milleja1@mail.gvsu.edu>
2020-02-02 17:44:41 -0500

    testing

--- HW3Calculator/SettingsViewController.swift
+++ HW3Calculator/SettingsViewController.swift
@@ -8,20 +8,81 @@
 
 import UIKit
 
+protocol SettingsViewControllerDelegate {
+    func settingsChangedLength(fromUnits: String, toUnits: String)
+    func settingsChangedVolume(fromUnits: String, toUnits: String)
+}
+
 class SettingsViewController: UIViewController {
     
     @IBOutlet weak var picker: UIPickerView!
+    var pickerData: [String] = [String]()
+    internal var isLengthMode: Bool = true
+    internal var unitLabel1 = UILabel()
+    internal var unitLabel2 = UILabel()
+    
+    @IBOutlet weak var button1: UIButton!
+    @IBOutlet weak var button2: UIButton!
+    
+    internal var delegate: SettingsViewControllerDelegate?
+
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        if (isLengthMode) {
+            pickerData = ["Meters", "Yards", "Miles"]
+        }
+        else {
+            pickerData = ["Gallons", "Liters", "Quarts"]
+        }
+        
+        button1.setTitle(unitLabel1.text, for: .normal)
+        button2.setTitle(unitLabel2.text, for: .normal)
     }
     
     @IBAction func onButton1Pressed(_ sender: Any) {
-        print("Button pressed")
+        picker.isHidden = false
     }
     
     @IBAction func onButton2Pressed(_ sender: Any) {
+        picker.isHidden = false;
+    }
+    
+//    @IBAction func savePressed(_ sender: UIBarButtonItem) {
+//
+//        if let del = self.delegate {
+//            //            del.settingsChanged(distanceUnits: self.dUnits!, bearingUnits: self.bUnits!)
+//            switch(mode) {
+//            case .Length:
+//                del.settingsChanged(fromUnits: LengthUnit(rawValue: fromUnits.text!)!, toUnits: LengthUnit(rawValue: toUnits.text!)!)
+//            case .Volume:
+//                del.settingsChanged(fromUnits: VolumeUnit(rawValue: fromUnits.text!)!, toUnits: VolumeUnit(rawValue: toUnits.text!)!)
+//            }
+//        }
+//        //self.dismiss(animated: true, completion: nil)
+//        _ = self.navigationController?.popViewController(animated: true)
+//    }
+
+    
+    @IBAction func onSavePressed(_ sender: Any) {
+        if let d = delegate {
+            d.settingsChangedLength(fromUnits: button1.currentTitle!, toUnits: button2.currentTitle!)
+            
+            performSegue(withIdentifier: "return", sender: sender)
+        }
+    }
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        if (segue.identifier ==  "save") {
+
+//            let vc = segue.destination as! ViewController
+//            vc.label1.text = button1.currentTitle
+//            vc.label2.text = button2.currentTitle
+
+//            delegate?.settingsChangedLength(fromUnits: button1.currentTitle!, toUnits: button2.currentTitle!)
+
+        }
     }
     
     /*
--- HW3Calculator/ViewController.swift
+++ HW3Calculator/ViewController.swift
@@ -8,7 +8,24 @@
 
 import UIKit
 
-class ViewController: UIViewController & UITextFieldDelegate {
+class ViewController: UIViewController, UITextFieldDelegate, SettingsViewControllerDelegate {
+    
+    func settingsChangedLength(fromUnits: String, toUnits: String) {
+        label1.text = fromUnits
+        label2.text = toUnits
+        fromInput.placeholder = "Enter length in \(fromUnits)"
+        toOutput.placeholder = "Enter length in Meters \(toUnits)"
+        
+        print(self.label1.text)
+    }
+    
+    func settingsChangedVolume(fromUnits: String, toUnits: String) {
+        label1.text = fromUnits
+        label2.text = toUnits
+        fromInput.placeholder = "Enter volume in \(fromUnits)"
+        toOutput.placeholder = "Enter volume in Meters \(toUnits)"
+    }
+    
     
     @IBOutlet weak var fromInput: UITextField!
     @IBOutlet weak var toOutput: UITextField!
@@ -18,6 +35,7 @@ class ViewController: UIViewController & UITextFieldDelegate {
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        
         // Do any additional setup after loading the view.
         let dectTouch = UITapGestureRecognizer(target: self, action: #selector(self.dismissKeyboard))
         self.view.addGestureRecognizer(dectTouch)
@@ -68,6 +86,22 @@ class ViewController: UIViewController & UITextFieldDelegate {
             fromInput.placeholder = "Enter length in Yards"
             toOutput.placeholder = "Enter length in Meters"
         }
+    }
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        let sVC = segue.destination as! SettingsViewController
+        sVC.delegate = self
+        
+        if (labelTitle.text?.contains("Length") == true) {
+            sVC.isLengthMode = true
+        }
+        else
+        {
+            sVC.isLengthMode = false
+        }
+        
+        sVC.unitLabel1 = label1
+        sVC.unitLabel2 = label2
     }
 }
 
